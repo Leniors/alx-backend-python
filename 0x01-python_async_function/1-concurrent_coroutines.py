5384a1926dd7512aaf0fa96f5a21c1534a2eafee
Leniors <mokualeeroy@gmail.com>
2024-05-06 23:39:47 +0300

    async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,8 +6,8 @@ task 1
 """
 import asyncio
 from typing import List
-from my_module import wait_random
 
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
