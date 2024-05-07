72425cef66f46973ba516072670e20a46fafadaa
Leniors <mokualeeroy@gmail.com>
2024-05-07 23:51:21 +0300

    python_async_comprehension

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import asyncio
+import random
+
+async def async_generator() -> [float]:
+    """
+    #!/usr/bin/env python3
+
+    import asyncio
+    import random
+    from typing import AsyncGenerator
+
+
+    async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
